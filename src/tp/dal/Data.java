94f239297b4cd18b047936fdd635ca30eadb8cd4
Cristian Pereyra <cristianpl99@gmail.com>
2023-05-12 14:07:31 -0300

    backend refactorizado

--- src/tp/dal/Data.java
+++ src/tp/dal/Data.java
@@ -4,38 +4,51 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import tp.logic.Employee;
+import tp.logic.Employee.Role;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.ArrayList;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+
 
 public class Data {
 	private String fileLocation = "src" + java.io.File.separator + "tp" + java.io.File.separator + "dal"
 			+ java.io.File.separator + "employeesList.json";
-	private String fileConflicts = "src" + java.io.File.separator + "tp" + java.io.File.separator + "dal"
-			+ java.io.File.separator + "conflicts.json";
 
-    public List<Employee> readEmployeesFromJSON() {
-        try (FileReader reader = new FileReader(fileLocation)) {
-            Gson gson = new Gson();
-            Type employeeListType = new TypeToken<List<Employee>>() {}.getType();
-            return gson.fromJson(reader, employeeListType);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-    
-    public List<List<Integer>> readTuplesFromJSON() {
-        try (FileReader reader = new FileReader(fileConflicts)) {
-            Gson gson = new Gson();
-            Type listType = new TypeToken<List<List<Integer>>>() {}.getType();
-            return gson.fromJson(reader, listType);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	public List<Employee> readEmployeesFromJSON() {
+	    try (FileReader reader = new FileReader(fileLocation)) {
+	        Gson gson = new Gson();
+	        Type employeeListType = new TypeToken<List<Map<String, Object>>>() {}.getType();
+	        List<Map<String, Object>> employeeMapList = gson.fromJson(reader, employeeListType);
+
+	        List<Employee> employees = new ArrayList<>();
+
+	        for (Map<String, Object> employeeMap : employeeMapList) {
+	            int id = ((Double) employeeMap.get("id")).intValue();
+	            String firstName = (String) employeeMap.get("firstName");
+	            String lastName = (String) employeeMap.get("lastName");
+	            int rating = ((Double) employeeMap.get("rating")).intValue();
+	            List<Double> idsConflicteds = (List<Double>) employeeMap.get("IdsConflicted");
+	            Role role = Role.valueOf((String) employeeMap.get("role"));
+	            String photo = (String) employeeMap.get("photo");
+
+	            Set<Double> idsConflictedsSet = new HashSet<>(idsConflicteds);
+
+	            Employee employee = new Employee(id, firstName, lastName, rating, idsConflictedsSet, role, photo);
+	            employees.add(employee);
+	        }
+
+	        return employees;
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    return null;
+	}
 }
 
--- src/tp/logic/Employee.java
+++ src/tp/logic/Employee.java
@@ -1,21 +1,35 @@
 package tp.logic;
 
+import java.util.Set;
+
 public class Employee {
 	private int id;
     private String firstName;
     private String lastName;
     private int rating;
+    private Set<Double> idsConflicteds;
     private Role role;
+    private String photo;
 
-    public Employee(int id, String firstName, String lastName, int rating, Role role) {
+    public Employee(int id, String firstName, String lastName, int rating, Set<Double> conflicts, Role role, String photo) {
     	this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.rating = rating;
+        this.idsConflicteds = conflicts;
         this.role = role;
+        this.photo = photo;
     }
     
-    public int getId() {
+    public String getPhoto() {
+		return photo;
+	}
+
+	public Set<Double> getConflicts() {
+		return idsConflicteds;
+	}
+
+	public int getId() {
         return id;
     }
 
