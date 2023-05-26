1f7869961f1e7aa5f9f4178fb91f3ed9693bb61b
Cristian Pereyra <cristianpl99@gmail.com>
2023-05-26 12:28:49 -0300

    tabla agregada como ejemplo

--- src/tp/gui/ComparisonScreen.java
+++ src/tp/gui/ComparisonScreen.java
@@ -16,9 +16,21 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.renderer.category.BarRenderer;
+
+
 import tp.logic.Employee;
 
 import java.util.List;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class ComparisonScreen extends JFrame {
 
@@ -39,7 +51,7 @@ public class ComparisonScreen extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		String[] columnNames = { "DNI", "Role", "Name", "Lastname", "Rating" };
+		String[] columnNames = { "DNI", "Role", "Lastname", "Rating" };
 
 		for (int i = 0; i < 3; i++) {
 			DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
@@ -69,7 +81,7 @@ public class ComparisonScreen extends JFrame {
 			}
 			JScrollPane scrollPane = new JScrollPane(table);
 			
-			scrollPane.setBounds(10 + i * 290, 35, 265, 292); // 265 ancho de cada tabla
+			scrollPane.setBounds(15 + i * 290, 35, 280, 260); 
 			contentPane.add(scrollPane);
 		}
 	
@@ -97,6 +109,29 @@ public class ComparisonScreen extends JFrame {
 	contentPane.add(scrollPane);
 	populateResultsTable(table);
 	contentPane.setLayout(null);
+	
+	JPanel graphPanel = new JPanel();
+	graphPanel.setBounds(20, 319, 387, 137);
+	contentPane.add(graphPanel);
+	createBarChart(resultMap, graphPanel);
+	
+	JLabel lblBruteForce = new JLabel("Brute Force");
+	lblBruteForce.setFont(new Font("Tahoma", Font.BOLD, 14));
+	lblBruteForce.setHorizontalAlignment(SwingConstants.CENTER);
+	lblBruteForce.setBounds(76, 11, 123, 22);
+	contentPane.add(lblBruteForce);
+	
+	JLabel lblBacktracking = new JLabel("Backtracking");
+	lblBacktracking.setHorizontalAlignment(SwingConstants.CENTER);
+	lblBacktracking.setFont(new Font("Tahoma", Font.BOLD, 14));
+	lblBacktracking.setBounds(389, 11, 123, 22);
+	contentPane.add(lblBacktracking);
+	
+	JLabel lblHeurisctic = new JLabel("Heuristic");
+	lblHeurisctic.setHorizontalAlignment(SwingConstants.CENTER);
+	lblHeurisctic.setFont(new Font("Tahoma", Font.BOLD, 14));
+	lblHeurisctic.setBounds(663, 11, 123, 22);
+	contentPane.add(lblHeurisctic);
 	}
 	
 	public void initialize() {
@@ -114,8 +149,8 @@ public class ComparisonScreen extends JFrame {
         
         List<Employee> employees = (List<Employee>) resultMap.get(key)[0];
         for (Employee employee : employees) {
-            model.addRow(new Object[] { employee.getDni(), employee.getRole(), employee.getFirstName(),
-                    employee.getLastName(), employee.getRating() });
+            model.addRow(new Object[] { employee.getDni(), employee.getRole(),
+                    					employee.getLastName(), employee.getRating() });
         }
     }
 	
@@ -129,24 +164,46 @@ public class ComparisonScreen extends JFrame {
 	        long time = (long) values[2];
 	        double executionTimeSeconds = (TimeUnit.MILLISECONDS.toSeconds(time)
 	                + (time % 1000) / 1000.0);
-	        String executionTime = String.format("%.3fs", executionTimeSeconds);
-	        String averageRating = String.format("%.3fs",values[3]);
+	        String executionTime = String.format("%.4fs", executionTimeSeconds);
+	        String averageRating = String.format("%.4f",values[3]);
 	        model.addRow(new Object[] { algorithm, combinations, executionTime, averageRating });
 	    }
 	}
-
-
-
 	
-	
-	/**
-	private void showEmployee(int selectedRow) {
-		EmployeeScreen launch;
-		launch = new EmployeeScreen(this.bestCombination.get(selectedRow));
-		launch.setResizable(false);
-		launch.setVisible(true);
-		launch.setLocationRelativeTo(null);
-	}
-	**/
 
+private void createBarChart(Map<String, Object[]> resultMap, JPanel graphPanel) {
+    DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+    for (Map.Entry<String, Object[]> entry : resultMap.entrySet()) {
+        String algorithm = entry.getKey();
+        double averageRating = (double) entry.getValue()[3];
+        dataset.addValue(averageRating, "Average Rating", algorithm);
+    }
+
+    JFreeChart chart = ChartFactory.createBarChart(
+            "Team Rating Comparison", 
+            "Algorithm", 
+            "Average Rating", 
+            dataset,
+            PlotOrientation.VERTICAL, 
+            true, 
+            true, 
+            false 
+    );
+    
+    CategoryPlot plot = chart.getCategoryPlot();
+    plot.setBackgroundPaint(java.awt.Color.GRAY); 
+    plot.getRangeAxis().setRange(3, 5);
+
+    BarRenderer renderer = (BarRenderer) plot.getRenderer();
+    renderer.setSeriesPaint(0, java.awt.Color.BLUE);
+
+    ChartPanel chartPanel = new ChartPanel(chart);
+    chartPanel.setPreferredSize(graphPanel.getSize());
+    graphPanel.removeAll();
+    graphPanel.setLayout(new java.awt.FlowLayout());
+    graphPanel.add(chartPanel);
+    graphPanel.revalidate();
+    graphPanel.repaint();
+}
 }
